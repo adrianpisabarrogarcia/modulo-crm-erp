44b0d77dfbe29d8da1be7f3fc87ed5c7aceb6e37
adrianpisabrrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-01-28 00:10:02 +0100

    Estructura y menú hechos

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
-print("hola mundo")
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('oportunidades.html')
+
+@app.route('/etapas')
+def etapas():
+    return render_template('etapas.html')
+
+@app.route('/clientes')
+def clientes():
+    return render_template('clientes.html')
+
+@app.route('/informes')
+def informes():
+    return render_template('informes.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
