275e584857c4a594e51e743eb1a9578655fd8310
Adrian Pisabarro García <apisabarro@teknei.com>
2022-02-11 10:18:31 +0100

    Añadir cliente, modificar cliente, eliminar cliente > ok todo

--- controller/c_cliente.py
+++ controller/c_cliente.py
@@ -1,5 +1,3 @@
-from flask import jsonify
-
 from model.cliente import Cliente
 
 clientes = []
@@ -27,12 +25,37 @@ def anadir_cliente(request):
 def listar_clientes():
     return clientes
 
-def eliminar_cliente():
+def eliminar_cliente(id):
     for cliente in clientes:
-        if cliente.id == id:
+        id_cliente = str(cliente.id)
+        id = str(id)
+        if id_cliente == id:
             clientes.remove(cliente)
-            return "Cliente eliminado"
-    return "No se encontró el cliente"
 
-def modificar_cliente():
-    print("modificar clientes")
+    i = 0
+    for cliente in clientes:
+        i += 1
+        cliente.id = i
+
+
+def modificar_cliente(request):
+    id = str(request.form.get("id-modificar"))
+    nombre = str(request.form.get("nombre-modificar"))
+    telefono = str(request.form.get("telefono-modificar"))
+    web = str(request.form.get("web-modificar"))
+    direccion = str(request.form.get("direccion-modificar"))
+    ciudad = str(request.form.get("ciudad-modificar"))
+    email = str(request.form.get("email-modificar"))
+
+    cliente = Cliente(id, nombre, telefono, email, web, direccion, ciudad)
+
+    for cliente_existente in clientes:
+        if cliente_existente.id == str(cliente.id):
+            cliente_existente.nombre = cliente.nombre
+            cliente_existente.telefono = cliente.telefono
+            cliente_existente.web = cliente.web
+            cliente_existente.direccion = cliente.direccion
+            cliente_existente.ciudad = cliente.ciudad
+            cliente_existente.email = cliente.email
+            print("Cliente modificado: "+str(cliente.id))
+            break
--- index.py
+++ index.py
@@ -3,11 +3,16 @@ from controller.c_cliente import anadir_cliente, listar_clientes, eliminar_clien
 
 app = Flask(__name__)
 
+########################################################################################################################
+# Oportunidades: Mostrar
 @app.route('/')
 def home():
     request.args.get('page')
     return render_template('oportunidades.html')
 
+
+########################################################################################################################
+# Clientes: Mostrar y Añadir
 @app.route('/clientes', methods=["GET", "POST"])
 def clientes():
     clientes = listar_clientes()
@@ -16,9 +21,32 @@ def clientes():
 
     return render_template('clientes.html', clientes=clientes)
 
+# Clientes: Modificar
+@app.route('/clientes/edit', methods=["GET", "POST"])
+def edit_clientes():
+    if request.method == "POST":
+        modificar_cliente(request)
+
+    return redirect(url_for('clientes'))
+
+# Clientes: Eliminar
+@app.route('/clientes/delete')
+def delete_clintes():
+    if request.method == "GET":
+        id = str(request.args.get("id"))
+        print(id)
+        eliminar_cliente(id)
+    return redirect(url_for('clientes'))
+
+########################################################################################################################
+# Informes: Mostrar
 @app.route('/informes')
 def informes():
     return render_template('informes.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
